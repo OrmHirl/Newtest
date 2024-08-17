b2e4b99a4c0c3cf246e1ddf2f6340fe635573a82
OrmHirl <151443035+OrmHirl@users.noreply.github.com>
2024-08-18 00:59:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -29,6 +29,67 @@ class JavaHeader extends HTMLElement {
             <div class="menu__burger">
                 <span></span>
             </div>
+            <nav class="navbar">
+                <ul class="navbar__list">
+                    <li><a href="nuntia.html" class="navbar__link">Nuntia</a></li>
+                    <li><a href="sodalitas.html" class="navbar__link">Sodalitas</a>
+                        <span class="navbar__arrow"></span>
+                        <ul class="navbar__sublist">
+                            <li><a href="administration.html" class="navbar__sublink">Administratio</a></li>
+                            <li><a href="legislation.html" class="navbar__sublink">Lex</a></li>
+                            <li><a href="statistics.html" class="navbar__sublink">Statisticum</a></li>
+                            <li><a href="dignitatum.html" class="navbar__sublink">Notitia Dignitatum</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="apopsis.html" class="navbar__link">Apopsis</a>
+                        <span class="navbar__arrow"></span>
+                        <ul class="navbar__sublist">
+                            <li><a href="methodology.html" class="navbar__sublink">Methodologia</a></li>
+                            <li><a href="articles.html" class="navbar__sublink">Articuli</a></li>
+                            <li><a href="polemics.html" class="navbar__sublink">Polemikos</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="regnum.html" class="navbar__link">Regnum</a>
+                        <span class="navbar__arrow"></span>
+                        <ul class="navbar__sublist">
+                            <li><a href="hist_koryanna.html" class="navbar__sublink">Historia Coriannae</a></li>
+                            <li><a href="hist_igf.html" class="navbar__sublink">Historia Praetorianae</a></li>
+                            <li><a href="government.html" class="navbar__sublink">Rectio</a></li>
+                            <li><a href="praetorium.html" class="navbar__sublink">Praetorium</a></li>
+                            <li><a href="economy.html" class="navbar__sublink">Oeconomia</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="pandektes.html" class="navbar__link">Pandektes</a>
+                        <span class="navbar__arrow"></span>
+                        <ul class="navbar__sublist">
+                            <li><a href="astrography.html" class="navbar__sublink">Astrographia</a></li>
+                            <li><a href="astropolitics.html" class="navbar__sublink">Astropolitika</a></li>
+                            <li><a href="documents.html" class="navbar__sublink">Documenta</a></li>
+                            <li><a href="military_encyclopedia.html" class="navbar__sublink">Magnum Lexicon Militare</a></li>
+                            <li><a href="linguistics.html" class="navbar__sublink">Linguisticus</a></li>
+                            <li><a href="mysticism.html" class="navbar__sublink">Mystica</a></li>
+                            <li><a href="prosopography.html" class="navbar__sublink">Prosopographia</a></li>
+                            <li><a href="technopedia.html" class="navbar__sublink">Technopaedia</a></li>
+                            <li><a href="uniformology.html" class="navbar__sublink">Uniformologia</a></li>
+                            <li><a href="chronology.html" class="navbar__sublink">Chronologia</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="cultura.html" class="navbar__link">Cultura</a>
+                        <span class="navbar__arrow"></span>
+                        <ul class="navbar__sublist">
+                            <li><a href="library.html" class="navbar__sublink">Librarium</a></li>
+                            <li><a href="gallery.html" class="navbar__sublink">Galeria</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="civitas.html" class="navbar__link">Civitas</a>
+                        <span class="navbar__arrow"></span>
+                        <ul class="navbar__sublist">
+                            <li><a href="forum.html" class="navbar__sublink">Forum</a></li>
+                            <li><a href="links.html" class="navbar__sublink">Nexus</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
         </header>
         `
     }
@@ -45,13 +106,13 @@ class JavaFooter extends HTMLElement {
             </div>
             <div class="footer__md">
                 <div class="footer__visuals">
-                    <div class="footer__logo"><img src="img/footer__logo.png"></div>
-                    <div class="footer__logo__s"><img src="img/footer__logo__s.png"></div>
-                    <div class="footer__dataport"><img src="img/footer__dataport__l.png"></div>
-                    <div class="footer__dataport__s"><img src="img/footer__dataport__s.png"></img></div>
-                    <div class="footer__dataport__ss"><img src="img/footer__dataport__ss.png"></img></div>
-                    <div class="footer__btns"><img src="img/footer__buttons.png"></div>
-                    <div class="footer__btn"><img src="img/footer__button.png"></div>
+                    <div class="footer__logo"><img src="img/footer__logo.webp"></div>
+                    <div class="footer__logo__s"><img src="img/footer__logo__s.webp"></div>
+                    <div class="footer__dataport"><img src="img/footer__dataport__l.webp"></div>
+                    <div class="footer__dataport__s"><img src="img/footer__dataport__s.webp"></img></div>
+                    <div class="footer__dataport__ss"><img src="img/footer__dataport__ss.webp"></img></div>
+                    <div class="footer__btns"><img src="img/footer__buttons.webp"></div>
+                    <div class="footer__btn"><img src="img/footer__button.webp"></div>
                 </div>
             </div>
             <div class="footer__rt"></div>
@@ -62,3 +123,58 @@ class JavaFooter extends HTMLElement {
 
 customElements.define('templ-header', JavaHeader)
 customElements.define('templ-footer', JavaFooter)
+
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () { 
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+	iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+	Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+	Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+	any: function() {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+
+    let menuArrows = document.querySelectorAll('.navbar__arrow');
+    if (menuArrows.length > 0) {
+        for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", function (e) {
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+    }
+
+} else {
+    document.body.classList.add('_pc');
+}
+
+const burgerMenu = document.querySelector('.menu__burger');
+if (burgerMenu) {
+    const navbarMenu = document.querySelector('.navbar');
+    burgerMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        burgerMenu.classList.toggle('_active');
+        navbarMenu.classList.toggle('_active');
+    });
+}
